1a491442b59ccb8def72fd95b57f2cce58c1a731
Alena Katkova <alena.katkova1996@gmail.com>
2023-07-17 23:28:02 +0300

    setting gitignore

---
+++ src/test/java/VacationServiceTest.java
@@ -0,0 +1,35 @@
+import org.junit.Assert;
+import org.junit.Test;
+import ru.netology.services.VacationService;
+
+public class VacationServiceTest {
+    @Test
+    public void testCalculateCount1() {
+        VacationService vacationService = new VacationService();
+        int income = 10_000;
+        int expenses = 3000;
+        int threshold = 20_000;
+
+        int actual = vacationService.calculateCount(income, expenses, threshold);
+        int expected = 3;
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testCalculateCount2() {
+        VacationService vacationService = new VacationService();
+        int income = 100_000;
+        int expenses = 60_000;
+        int threshold = 150_000;
+
+        int actual = vacationService.calculateCount(income, expenses, threshold);
+        int expected = 2;
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+
+}
